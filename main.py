ccb4829870f0023d7172d05cf59ebb4f95c0cc36
josuecastro <josue.castro@upr.edu>
2018-03-28 12:43:12 -0400

    contact route added

--- main.py
+++ main.py
@@ -1,36 +1,49 @@
-from flask import Flask
+from flask import Flask, render_template
 from handlers.messages import MessageHandler
 from handlers.contacts import ContactHandler
-app = Flask(__name__)
+
+app = Flask(__name__, template_folder='template')
 
 
 @app.route('/')
 def home():
-    return "Hello World"
+    return "Welcome to Message App"
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 
 @app.route('/login')
 def login():
-    return "Login page"
+    return render_template('login.html')
+
 
 @app.route('/groups')
 def groups():
     return 0
 
+
 @app.route('/groups/<int:gid>/members')
 def members():
     return 0
+
 
 @app.route('/group/<int:gid>/messages')
 def getGroupMessages(gid):
     return MessageHandler().getGroupMessages(gid)
 
+
 @app.route('/group/<int:gid>/messages/by/<int:pid>')
 def getMessageBySender(gid, pid):
     return MessageHandler().getMessagesBySender(gid, pid)
 
+
 @app.route('/<int:pid>/contacts')
 def getMyContacts(pid):
     return ContactHandler().getMyContacts(pid)
+
 
 @app.route('/<int:pid>/contacts/<string:name>')
 def getContactByName(pid, name):
