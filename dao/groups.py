6620f2ff4d31dee5f92fa15b097f39d29e98aa94
EdgardooAndres <edgardo.muniz@upr.edu>
2018-04-17 22:35:31 -0400

    Groups query working with local dummy data in postgres.

--- dao/groups.py
+++ dao/groups.py
@@ -1,10 +1,16 @@
+import psycopg2
+
+from config.dbconfig import pg_config
+
+
 class GroupsDAO:
     def __init__(self):
 
-        #connection_url = "dbname=%s user=%s password=%s" % (pg_config['dbname'],
-        #                                                    pg_config['user'],
-        #                                                    pg_config['passwd'])
-        #self.conn = psycopg2._connect(connection_url)
+        # connection_url = "dbname=%s user=%s password=%s" % (pg_config['postgres'],
+        #                                                    pg_config['eggiemuniz'],
+        #                                                     pg_config['andres'])
+
+        self.conn = psycopg2.connect(host="localhost", database="postgres", user="eggiemuniz", password="andres")
 
         #     gid,  gName,     admin
         G1 = [101, 'Grupo DB', 120]
@@ -17,15 +23,13 @@ class GroupsDAO:
         self.groups.append(G3)
 
     def getAllGroups(self):
-        return self.groups
-
-        # cursor = self.conn.cursor()
-        # query = "select * from GroupChat;"
-        # cursor.execute(query)
-        # result = []
-        # for row in cursor:
-        #     result.append(row)
-        # return result
+        cursor = self.conn.cursor()
+        query = "SELECT * FROM groupchat;"
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
 
     def getGroupById(self, gid):
         result = []
