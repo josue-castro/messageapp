f13885614a9ef731c3126e6f2ad25d1f7aad22ab
josuecastro <josue.castro@upr.edu>
2018-05-28 21:58:34 -0400

    arregle adding members

--- handlers/members.py
+++ handlers/members.py
@@ -36,18 +36,24 @@ class MemberHandler:
                 member_dao = MembersDAO()
                 user_dao = UserDAO()
                 group_dao = GroupsDAO()
-                if gid in group_dao.getAllGroupsAdminByUserINFO(pid)[0]:
-                    user_id = user_dao.getUserByUsername(username)[0]
-                    if not user_id:
-                        return jsonify(Error="User does not exist"), 400
-                    elif gid not in user_dao.getUserGroups(user_id)[0]:
-                        member_dao.insertMember(gid, user_id)
-                        result = self.build_member_attributes(gid, user_id)
-                        return jsonify(new_Member=result), 201
+                if group_dao.getAllGroupsAdminByUserINFO(pid):
+                    if gid in group_dao.getAllGroupsAdminByUserINFO(pid)[0]:
+                        if user_dao.getUserByUsername(username):
+                            user_id = user_dao.getUserByUsername(username)[0]
+                            if not user_dao.getUserGroups(user_id):
+                                member_dao.insertMember(gid, user_id)
+                                result = self.build_member_attributes(gid, user_id)
+                                return jsonify(new_Member=result), 201
+                            elif gid not in user_dao.getUserGroups(user_id)[0]:
+                                member_dao.insertMember(gid, user_id)
+                                result = self.build_member_attributes(gid, user_id)
+                                return jsonify(new_Member=result), 201
+                            else:
+                                return jsonify(Error="User is already a member"), 400
+                        else:
+                            return jsonify(Error="User does not exist"), 400
                     else:
-                        return jsonify(Error="User is already a member"), 400
-                else:
-                    return jsonify(Error="You are not administrator of group"), 400
+                        return jsonify(Error="You are not administrator of group"), 400
             else:
                 return jsonify(Error="Unexpected attributes in post request"), 400
 
